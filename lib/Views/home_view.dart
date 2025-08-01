9bec20e4c9f39e1a109af754b97877ab92b281de
alikasrawy <kasrawyali47@gmail.com>
2025-08-01 11:40:25 +0300

    create home view file

---
+++ lib/Views/home_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Home View", style: TextStyle(
+      fontSize: 30,
+      fontWeight: FontWeight.bold
+    ),),);
+  }
+}
