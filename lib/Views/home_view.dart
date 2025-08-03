1484f85b8b9fc5cd8398fbd1739c9d200101af98
alikasrawy <kasrawyali47@gmail.com>
2025-08-03 11:29:03 +0300

    create latest arrivals widget

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/Widgets/custom_swiper_to_home_view.dart';
+import 'package:e_commerce/Widgets/latest_arrivals.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,6 +8,31 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Column(children: [CustomSwiperToHomeView(size: size)]);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        CustomSwiperToHomeView(size: size),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Text(
+            "Latest arrivals",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
+        SizedBox(height: 30),
+        SizedBox(
+          height: size.height * 0.2,
+
+          child: ListView.builder(
+            itemCount: 10,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index) {
+              return LatestArrivals();
+            },
+          ),
+        ),
+      ],
+    );
   }
 }
---
+++ lib/Widgets/latest_arrivals.dart
@@ -0,0 +1,78 @@
+import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
+import 'package:flutter/material.dart';
+
+class LatestArrivals extends StatelessWidget {
+  const LatestArrivals({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      child: SizedBox(
+        width: size.width * 0.44,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: FancyShimmerImage(
+                  imageUrl:
+                      "https://i.ibb.co/8r1Ny2n/20-Nike-Air-Force-1-07.png",
+
+                  height: size.width * 0.44,
+                  width: size.width * 0.44,
+                ),
+              ),
+            ),
+            SizedBox(width: 7),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Title" * 10,
+                    style: TextStyle(fontSize: 18),
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  FittedBox(
+                    child: Row(
+                      children: [
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.favorite, color: Colors.red),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: Icon(
+                            Icons.add_shopping_cart_rounded,
+                            color: Colors.red,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  Text(
+                    "150\$",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.blue,
+                      fontWeight: FontWeight.bold,
+                    ),
+
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  SizedBox(height: 10),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
