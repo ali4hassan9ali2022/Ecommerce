b73fbd0f753ebd962a6c9a7b8355c9554f19a9cb
alikasrawy <kasrawyali47@gmail.com>
2025-09-10 23:10:42 +0300

    add three  state add  product to cart to search state

--- lib/Cubit/Search_cubit/search_state.dart
+++ lib/Cubit/Search_cubit/search_state.dart
@@ -1,5 +1,4 @@
 import 'package:e_commerce/Models/products_models.dart';
-
 abstract class SearchState {}
 
 class SearchInitial extends SearchState {}
@@ -16,4 +15,13 @@ class FailureSearchState extends SearchState {
   final String errMessage;
 
   FailureSearchState({required this.errMessage});
+}
+class LoadingAddProductToCartState extends SearchState{}
+class SuccessAddProductToCartState extends SearchState{
+  late final List<Map<String,dynamic>> productsInCart;
+}
+class FailureAddProductToCartState extends SearchState{
+  final String errMessage;
+
+  FailureAddProductToCartState({required this.errMessage});
 }
