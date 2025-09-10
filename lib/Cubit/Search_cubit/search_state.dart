2eab7db67c434aec0b356c8cb9054b012cad4994
alikasrawy <kasrawyali47@gmail.com>
2025-09-10 23:00:04 +0300

    add there state to search state

--- lib/Cubit/Search_cubit/search_state.dart
+++ lib/Cubit/Search_cubit/search_state.dart
@@ -1,2 +1,19 @@
+import 'package:e_commerce/Models/products_models.dart';
+
 abstract class SearchState {}
+
 class SearchInitial extends SearchState {}
+
+class LoadingSearchState extends SearchState {}
+
+class SuccessSearchState extends SearchState {
+  final ProductsModels products;
+
+  SuccessSearchState({required this.products});
+}
+
+class FailureSearchState extends SearchState {
+  final String errMessage;
+
+  FailureSearchState({required this.errMessage});
+}
